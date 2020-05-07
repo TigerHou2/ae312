1a33ca2b0d549bb57460657ac96a1db61200b82f
TigerHou2 <44105132+TigerHou2@users.noreply.github.com>
2020-05-06 23:12:26 -0500

    Compiled HW8

--- HW8/HW8.m
+++ HW8/HW8.m
@@ -123,3 +123,21 @@ latexify(19,12)
 % ------------------------------
 % ---- Part b
 % ------------------------------
+
+
+
+%% Function Definitions
+
+function [cpu,cpl] = cp_Joukowski(x,alpha,tc,M)
+%CP_JOUKOWSKI Summary of this function isn't here
+%   why is a bicycle slower than a tricycle?
+%   because it's two-tired.
+
+c = max(x)-min(x);
+t = c * tc;
+theta = c .* ( (77.*t.*x.*((2.*x)./c - 1))./(50.*c.^3.*(1 - (4.*x.^2)./c.^2).^(1./2)) - (77.*t.*(1 - (4.*x.^2)./c.^2).^(1./2))./(100.*c.^2));
+theta_u = theta - alpha;
+theta_l = -theta - alpha;
+cpu = 2.*theta_u / sqrt(M^2-1);
+cpl = 2.*theta_l / sqrt(M^2-1);
+end
